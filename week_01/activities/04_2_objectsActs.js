da4cf6e17bf7565a084b4988db851497612554d1
Indykatz <cats83@me.com>
2022-05-13 15:23:00 +0100

    edited version

--- week_01/activities/04_2_objectsActs.js
+++ week_01/activities/04_2_objectsActs.js
@@ -44,15 +44,18 @@ console.log(pet.drink());
 // coffee shop challange
 
 let total = 0;
+let finalOrder = [];
 
 const coffeeShop = {
   drinks: {
     coffee: 2.5,
     tea: 2,
+    milk: 1,
   },
   food: {
     sandwich: 5,
     toastie: 5,
+    soup: 4,
   },
 
   // drinks order
@@ -60,18 +63,20 @@ const coffeeShop = {
     // for each item in list
     for (let i = 0; i < orderList.length; i++) {
       if (this.drinks.hasOwnProperty(orderList[i])) {
-        console.log(`${orderList[i]}: £${this.drinks[orderList[i]]}`);
+        finalOrder.push(` ${orderList[i]}: £${this.drinks[orderList[i]]}`);
+        // console.log(`${orderList[i]}: £${this.drinks[orderList[i]]}`);
         total = total + this.drinks[orderList[i]];
       }
     }
   },
 
-  // drinks order
+  // food order
   foodOrdered(orderList) {
     // for each item in list
     for (let i = 0; i < orderList.length; i++) {
       if (this.food.hasOwnProperty(orderList[i])) {
-        console.log(`${orderList[i]}: £${this.food[orderList[i]]}`);
+        finalOrder.push(` ${orderList[i]}: £${this.food[orderList[i]]}`);
+        // console.log(`${orderList[i]}: £${this.food[orderList[i]]}`);
         total = total + this.food[orderList[i]];
       }
     }
@@ -81,10 +86,10 @@ const coffeeShop = {
   customerOrder(orderList) {
     this.drinksOrdered(orderList);
     this.foodOrdered(orderList);
-    console.log(`Total: £${total}`);
+    console.log(`Your order is${finalOrder}, comes to £${total}`);
   },
 };
 
-theOrder = ["tea", "coffee", "tea", "sandwich", "toastie", "toastie"];
+theOrder = ["tea", "coffee", "tea", "sandwich", "toastie", "soup"];
 
 coffeeShop.customerOrder(theOrder);
